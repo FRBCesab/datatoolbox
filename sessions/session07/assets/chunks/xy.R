f9c799dc74592c4eac008c0094f1fb47db148169
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-11-25 17:39:46 +0100

    Add Reprex Section

---
+++ sessions/session07/assets/chunks/xy.R
@@ -0,0 +1,13 @@
+#' @title Exemple d'un jeu de donnees interne
+#' @description Un jeu de donnees fictif illustrant la construction d'un
+#' exemple reproductible interne
+#'
+#' @format Un data frame avec 9 lignes et 2 colonnes :
+#' \describe{
+#'   \item{x}{valeurs tirees aleatoirement dans une distribution normale}
+#'   \item{y}{sequences de 2 a 9 avec une valeur manquante}
+#' }
+#' @source Random Number Generator (RNG): 1637
+#'
+
+"xy"
