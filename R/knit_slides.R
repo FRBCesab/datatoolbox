b089299516919505ae76f038613353e4fda9eda0
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-11-08 10:36:44 +0100

    :boom: New feature: knit_slides()

---
+++ R/knit_slides.R
@@ -0,0 +1,90 @@
+#' Knit Slides
+#'
+#' This function re-knits Rmd slides to HTML and PDF outputs (stored in the
+#'   same folder). You need to have installed the Google Chrome web browser to
+#'   create the PDF version. All the Rmd files need to be named **index.Rmd**.
+#'
+#' @param label the course/exercise subfolder name (e.g. "intro-git").
+#' @param home a boolean. If TRUE, only the Homepage will be re-knit.
+#' @param course a boolean. If TRUE, searchs in the "courses" folder otherwise
+#'   searchs in the "exercises" folder. Ignore if `home = TRUE`.
+#' @param pdf a boolean. If TRUE, converts HTML to PDF.
+#'
+#' @export
+#'
+#' @importFrom here here
+#' @importFrom pagedown chrome_print
+#' @importFrom rmarkdown render
+#' @importFrom usethis ui_todo ui_done ui_line
+#'
+#' @examples
+#'
+#' # Knit the Homepage ----
+#' knit_slides(home = TRUE)
+#'
+#' # Knit Git course ----
+#' knit_slides(label = "intro-git")
+#' knit_slides(label = "intro-git", pdf = FALSE)
+#'
+#' # Knit Git exercise ----
+#' knit_slides(label = "intro-git", course = FALSE)
+#' knit_slides(label = "intro-git", course = FALSE, pdf = FALSE)
+
+
+knit_slides <- function(label, home = FALSE, course = TRUE, pdf = FALSE) {
+
+  if (pdf && (nchar(pagedown::find_chrome()) == 0)) {
+    stop("Unable to find Chrome/Chromium web browser.")
+  }
+
+  if (home) {
+
+    usethis::ui_todo("Knitting Homepage slides...")
+
+    rmarkdown::render(
+      input       = here::here("index.Rmd"),
+      output_file = "index.html",
+      output_dir  = here::here(),
+      clean       = TRUE,
+      quiet       = TRUE
+    )
+
+    if (pdf) {
+      pagedown::chrome_print(
+        input  = here::here("index.html"),
+        output = here::here("index.pdf")
+      )
+    }
+
+  } else {
+
+
+    if (course) {
+      level <- "courses"
+    } else {
+      level <- "exercises"
+    }
+
+    usethis::ui_todo(paste("Knitting", label, "slides..."))
+
+    rmarkdown::render(
+      input       = here::here(level, label, "index.Rmd"),
+      output_file = "index.html",
+      output_dir  = here::here(level, label),
+      clean       = TRUE,
+      quiet       = TRUE
+    )
+
+    if (pdf) {
+      pagedown::chrome_print(
+        input  = here::here(level, label, "index.html"),
+        output = here::here(level, label, "index.pdf")
+      )
+    }
+  }
+
+  usethis::ui_done("Done!")
+  usethis::ui_line()
+
+  invisible(NULL)
+}
--- make.R
+++ make.R
@@ -5,7 +5,7 @@
 #' @usage source("make.R")
 #'
 #' @author Nicolas CASAJUS, \email{nicolas.casajus@@fondationbiodiversite.fr}
-#' @date 2020/11/07
+#' @date 2020/11/08
 
 
 
@@ -17,81 +17,22 @@ if (!("remotes" %in% installed.packages())) install.packages("remotes")
 remotes::install_deps(upgrade = "never")
 
 
-## Attach Required Packages (listed in `Depends` section in DESCRIPTION)    ----
+## Attach Required Packages and Load R Functions                            ----
 devtools::load_all(quiet = TRUE)
 
 
-## Re-knit {{ HOMEPAGE }}                                                   ----
-rmarkdown::render(
-  input       = here::here("index.rmd"),
-  output_file = "index.html",
-  output_dir  = here::here(),
-  clean       = TRUE,
-  quiet       = TRUE
-)
-usethis::ui_done("Knitting Homepage slides")
+## Re-knit COURSES                                                          ----
 
+knit_slides(label = "docker")
+knit_slides(label = "drake", pdf = TRUE)
+knit_slides(label = "intro-api")
+knit_slides(label = "intro-git", pdf = TRUE)
+knit_slides(label = "open-sci", pdf = TRUE)
+knit_slides(label = "r-pkg")
+knit_slides(label = "r-spatial", pdf = TRUE)
+knit_slides(label = "res-compendium", pdf = TRUE)
+knit_slides(label = "rmarkdown", pdf = TRUE)
+knit_slides(label = "take-home")
+knit_slides(label = "tidyverse", pdf = TRUE)
 
-## Re-knit {{ TAKE HOME }}                                                  ----
-rmarkdown::render(
-  input       = here::here("courses", "take-home", "index.rmd"),
-  output_file = "index.html",
-  output_dir  = here::here("courses", "take-home"),
-  clean       = TRUE,
-  quiet       = TRUE
-)
-usethis::ui_done("Knitting Take Home slides")
-
-
-## Re-knit {{ R PACKAGES }}                                                 ----
-rmarkdown::render(
-  input       = here::here("courses", "r-pkg", "index.rmd"),
-  output_file = "index.html",
-  output_dir  = here::here("courses", "r-pkg"),
-  clean       = TRUE,
-  quiet       = TRUE
-)
-usethis::ui_done("Knitting R Package slides")
-
-
-## Re-knit {{ DOCKER }}                                                     ----
-rmarkdown::render(
-  input       = here::here("courses", "docker", "index.rmd"),
-  output_file = "index.html",
-  output_dir  = here::here("courses", "docker"),
-  clean       = TRUE,
-  quiet       = TRUE
-)
-usethis::ui_done("Knitting Docker slides")
-
-
-## Re-knit {{ GIT }}                                                        ----
-rmarkdown::render(
-  input       = here::here("courses", "intro-git", "index.rmd"),
-  output_file = "index.html",
-  output_dir  = here::here("courses", "intro-git"),
-  clean       = TRUE,
-  quiet       = TRUE
-)
-usethis::ui_done("Knitting Git slides")
-
-
-## Re-knit {{ SPATIAL }}                                                        ----
-rmarkdown::render(
-  input       = here::here("courses", "r-spatial", "index.rmd"),
-  output_file = "index.html",
-  output_dir  = here::here("courses", "r-spatial"),
-  clean       = TRUE,
-  quiet       = TRUE
-)
-usethis::ui_done("Knitting Spatial slides")
-
-## Re-knit {{ API & WEBSCRAPPING }}                                         ----
-# rmarkdown::render(
-#   input       = here::here("courses", "intro-api", "index.rmd"),
-#   output_file = "index.html",
-#   output_dir  = here::here("courses", "intro-api"),
-#   clean       = TRUE,
-#   quiet       = TRUE
-# )
-# usethis::ui_done("Knitting API & Webscrapping slides")
+knit_slides(home = TRUE)
