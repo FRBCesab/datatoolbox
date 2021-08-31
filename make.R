71fa02a476b97d728c34f5853f0f81acb9df7ed1
Nicolas Casajus <nicolas.casajus@gmail.com>
2021-08-31 14:39:13 +0200

    :bug: Replace {icon} by {fontawesome}

--- make.R
+++ make.R
@@ -32,13 +32,11 @@ devtools::load_all(quiet = TRUE)
 
 datatoolbox::knit_slides(name = "docker")         ##
 datatoolbox::knit_slides(name = "drake")          ##
-datatoolbox::knit_slides(name = "intro-api")      ##
 datatoolbox::knit_slides(name = "intro-git")      ##
-# datatoolbox::knit_slides(name = "open-sci")
+# datatoolbox::knit_slides(name = "open-sci")     ..
 datatoolbox::knit_slides(name = "r-pkg")          ##
-# datatoolbox::knit_slides(name = "r-spatial")
 datatoolbox::knit_slides(name = "res-compendium") ##
-datatoolbox::knit_slides(name = "rmarkdown")
+datatoolbox::knit_slides(name = "rmarkdown")      ##
 datatoolbox::knit_slides(name = "take-home")      ##
 datatoolbox::knit_slides(name = "tidyverse")
 
